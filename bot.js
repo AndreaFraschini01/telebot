0c4973d28caeed247ae3943759fa338c054bafa9
AndreaFraschini01 <andrea.fraschini.studenti@isii.it>
2020-03-20 17:35:50 +0100

    aggiunta la funzionalità "rispondi che non vai" al bot

--- bot.js
+++ bot.js
@@ -1,3 +1,9 @@
-require("dotenv").config();
+require('dotenv').config();
+const Telegraf = require('telegraf');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-console.log(process.env.MESSAGE);
+bot.on('text', (ctx)=>{
+    ctx.reply('Ma salve! al momento non sono ancora programmato ma tra non molto lo sarò!')
+});
+
+bot.launch();
