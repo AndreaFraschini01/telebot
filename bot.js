2185af7ad73a7b5f6b3663553f7830f2a28b652d
AndreaFraschini01 <andrea.fraschini.studenti@isii.it>
2020-03-22 23:19:40 +0100

    Aggiunta i comandi /help /quote

--- bot.js
+++ bot.js
@@ -2,8 +2,20 @@ require('dotenv').config();
 const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.on('text', (ctx)=>{
-    ctx.reply('Ma salve! al momento non sono ancora programmato ma tra non molto lo sarò!')
+bot.command('quote', (ctx)=>{
+    if(ctx.message.reply_to_message){
+        
+        let {text, from, date} = ctx.message.reply_to_message;
+        let data = new Date(1000*date);
+        ctx.replyWithMarkdown(`_${text}_\n- ${from.username} ${data.toLocaleDateString()} ${data.toLocaleTimeString()}`);
+    }else{
+        ctx.reply("Si, ma linka un messagio bro");
+    }
+    
+});
+
+bot.help((ctx)=>{
+    ctx.reply("Rispondi ad un messaggio col comando /quote per salvare la citazione per i posteri!");
 });
 
 bot.launch();
