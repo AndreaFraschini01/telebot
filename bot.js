72e917cff9541beecc09280d1bc204c0128c1964
AndreaFraschini01 <andrea.fraschini.studenti@isii.it>
2020-04-04 11:45:00 +0200

    Ottimizzato /list e documentato il codice

--- bot.js
+++ bot.js
@@ -7,39 +7,48 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.command('list', (ctx)=>{
     let pagina = {c: 0};
-    try {
-        stampaLista(ctx, pagina);
-    } catch (error) {
-        console.log(error);
-    }
+    db.listaCitazioni(ctx.chat.id, function(res){
+        if(res.length == 0){
+            ctx.reply("Non ci sono citazioni salvate nella chat");
+        }
+        else{
+            stampaLista(ctx, res, pagina)
+        }
+    });
 })
 
 
 
 bot.command('quote', (ctx)=>{
-    if(ctx.message.reply_to_message){
+    if(ctx.message.reply_to_message){ //Controlla che il messaggio sia una risposta
+        //Ricava le informazioni che interessano (messaggio, mittente, data e id della chat)
         let {text, from, date, chat} = ctx.message.reply_to_message;
 
+        //Controlla la validità del messaggio
         if (text == undefined) {
             ctx.reply("Mi spiace, ma per adesso posso solo salvare testo 😥");
         }
         else{
             let idChat = chat.id;
             let author = from.username;
-            let data = new Date(1000*date);
 
+            //Formatta la data
+            let data = new Date(1000*date);
             data = `${data.toLocaleDateString()} ${data.toLocaleTimeString()}`;
 
+            //Inserisci la citazione nel database
             db.inserisciCitazione(idChat, {text, author, date: data}, function(modified){
-                if(modified == 0){
+                if(modified == 0){ //se non ci sono record modificati vuol dire che era già stata inserita
                     ctx.reply("Il messaggio è già stato citato");
                 }
                 else{
+                    //Rispondi con il messaggio formattato in markdown
                     ctx.replyWithMarkdown(`_${text}_\n- ${author.replace('_', '\\_')} ${data}`);
                 }
             });
         }
     }else{
+        //Gestione caso in cui il messaggio non sia una risposta
         ctx.reply("Si, ma linka un messagio bro");
     }
     
@@ -50,38 +59,38 @@ bot.help((ctx)=>{
     ctx.reply(helpmsg);
 });
 
-function stampaLista(ctx, pagina){
-    db.listaCitazioni(ctx.chat.id, pagina.c, function(res, next){
-        let msg = "";
-        if(res){
-            res.forEach((cit)=>{
-                msg+=`_${cit.text}_\n•${cit.author.replace('_', '\\_')} ${cit.date}\n\n`;
-            });
-            if(next.continues){
-                const message = ctx.replyWithMarkdown(msg, Markup.inlineKeyboard([
-                    Markup.callbackButton(`📖Altre citazioni... (${pagina.c+ 1}/${next.numPages})`, 'next')
-                ]).extra());
+function stampaLista(ctx, array, pagina){
+    
+    numPages = Math.ceil(array.length/5.0); //Calcola il numero delle pagine
+    let sliced = array.slice(pagina.c*5, (pagina.c + 1)*5); //Crea la pagina
 
-                bot.action('next', (ctx)=>{
-                    ctx.deleteMessage(message.message_id);
-                    pagina.c++;
-                    try {
-                        stampaLista(ctx, pagina);
-                    } catch (error) {
-                        console.log(error);
-                    }
-                    
-                })
-            }
-            else{
-                ctx.replyWithMarkdown(msg);
-                pagina.c = 0;
-            }
-        }
-        else{
-            ctx.reply("Non ci sono citazioni salvate in questa chat");
-        }
+    let msg = "";
+
+    //Crea il messaggio con la lista delle citazioni
+    sliced.forEach(cit => {
+        msg+=`_${cit.text}_\n•${cit.author.replace('_', '\\_')} ${cit.date}\n\n`;
     });
-}
+
+    if(pagina.c+1<numPages){
+
+        //Se non sei nell'ultima pagina stampa la lista con il bottone
+        const message = ctx.replyWithMarkdown(msg, Markup.inlineKeyboard([
+            Markup.callbackButton(`📖Altre citazioni... (${pagina.c+ 1}/${numPages})`, 'next')
+        ]).extra());
+
+        //Quando si preme il bottone
+        bot.action('next', function(ctx){
+            //Cancella la pagina precedente
+            ctx.deleteMessage(message.message_id);
+            pagina.c++;
+            stampaLista(ctx, array, pagina); //Ripeti finché non sei all'ultima pagina
+        });
+    }
+    else{
+        //Stampa l'ultima pagina senza bottone
+        ctx.replyWithMarkdown(msg);
+        pagina.c = 0; //Resetta il contatore delle pagine
+    }
+} 
 
 bot.launch();
--- database.js
+++ database.js
@@ -1,19 +1,25 @@
 var mongodb = require("mongodb");
 
+//Inserisci nel DB l'id di una chat con l'array di citazioni associato (tutti i casi sono gestiti)
 exports.inserisciCitazione = function (idChat, quote, callback){
     var mongoClient = mongodb.MongoClient(process.env.DB_URL, {useUnifiedTopology: true});
+
     mongoClient.connect(function(err, client){
         console.log("Inserimento citazione: collegamento...")
+
         if (err) {
             console.log("Impossibile connettersi al database", err);
         }
         else{
             console.log("Connessione al database stabilita");
+            //Ricava la collection
             var db = client.db(process.env.DB_NAME);
             var groups = db.collection("groups");
+
+            //Aggiorna solo un record
             groups.updateOne({_id: idChat}, 
                 { 
-                    $addToSet: { 
+                    $addToSet: {  //Tratta l'array come un insieme matematico (no duplicati)
                         quotes: {
                             text: quote.text,
                             author: quote.author, 
@@ -22,7 +28,7 @@ exports.inserisciCitazione = function (idChat, quote, callback){
                     } 
                 }, 
                 {
-                    upsert: true
+                    upsert: true  //Se non esiste il record, aggiungilo
                 }, 
                 function(err, res){
                     if(err){
@@ -30,6 +36,7 @@ exports.inserisciCitazione = function (idChat, quote, callback){
                         console.log(err);
                     }
                     else{
+                        //Ritorna il numero di record modificati
                         callback(res.modifiedCount);
                     }
                     client.close();
@@ -39,9 +46,10 @@ exports.inserisciCitazione = function (idChat, quote, callback){
     });
 }
 
-//Ritorna un array e il
-exports.listaCitazioni = function(idChat, pagina, callback){
+
+exports.listaCitazioni = function(idChat, callback){
     var mongoClient = mongodb.MongoClient(process.env.DB_URL, {useUnifiedTopology: true});
+
     mongoClient.connect(function(err, client){
         console.log("Recupero lista citazioni...")
         if (err) {
@@ -49,24 +57,20 @@ exports.listaCitazioni = function(idChat, pagina, callback){
         }
         else{
             console.log("Connessione al database stabilita");
+
+            //Ricava la collection
             var db = client.db(process.env.DB_NAME);
             var groups = db.collection("groups");
             
-            groups.find({_id: idChat}).project({_id: 0, quotes: {$slice:[pagina*5, 5]}})
-            .toArray(function(err, res){
-                if(err){
+            //Trova un solo record.
+            groups.findOne({_id: idChat}, function(err, res){
+                if (err) {
                     console.log(err);
                 }
                 else{
-                    if(res){
-                        console.dir(res);
-                        callback(res.quotes, {continues: true, numPages:5});
-                    }
-                    else{
-                        callback(null);
-                    }
+                    //Ritorna l'array di citazioni
+                    callback(res.quotes);
                 }
-                client.close();
             });
         }
     });
