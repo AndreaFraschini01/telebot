43320759c44823f3174f812e2f1e551825e9f827
AndreaFraschini01 <andrea.fraschini.studenti@isii.it>
2020-04-11 17:54:33 +0200

    RISOLTO IL PROBLEMA DEL CRISTO

--- bot.js
+++ bot.js
@@ -3,22 +3,7 @@ const Telegraf = require('telegraf');
 const Markup = require('telegraf/markup');
 var db = require('./database');
 const bot = new Telegraf(process.env.BOT_TOKEN)
-
-
-bot.command('list', (ctx)=>{
-    let pagina = {c: 0};
-    db.listaCitazioni(ctx.chat.id, function(res){
-        console.log({quotes_from_db:res});
-        if(res.length == 0){
-            ctx.reply("Non ci sono citazioni salvate nella chat");
-        }
-        else{
-            stampaLista(ctx, res, pagina)
-        }
-    });
-})
-
-
+var arraydicitazioni = [];
 
 bot.command('quote', (ctx)=>{
     if(ctx.message.reply_to_message){ //Controlla che il messaggio sia una risposta
@@ -55,44 +40,64 @@ bot.command('quote', (ctx)=>{
     
 });
 
-bot.help((ctx)=>{
-    let helpmsg="Rispondi ad un messaggio col comando /quote per salvare la citazione per i posteri!\nUtilizza il comando /list per vedere la lista delle citazioni";
-    ctx.reply(helpmsg);
+const tastoNext = Markup.inlineKeyboard([
+    Markup.callbackButton('next', 'n')
+]).extra()
+
+bot.command('list', (ctx)=>{
+    let pagina = {c: 0};
+    db.listaCitazioni(ctx.chat.id, function(res){
+        arraydicitazioni = res;
+        faiCose(ctx, arraydicitazioni, pagina);
+    });
 });
 
-function stampaLista(ctx, array, pagina){
+
+function faiCose(ctx, bar, page){
+
+    let numPages = Math.ceil(bar.length/5.0); //Calcola il numero delle pagine
+    let sliced = bar.slice(page.c*5, (page.c + 1)*5); //Crea la pagina
     
-    let numPages = Math.ceil(array.length/5.0); //Calcola il numero delle pagine
-    let sliced = array.slice(pagina.c*5, (pagina.c + 1)*5); //Crea la pagina
-    
-    //console.log(array);
+    console.log(`${page.c+1}/${numPages}`);
 
     let msg = "";
-
     //Crea il messaggio con la lista delle citazioni
     sliced.forEach(cit => {
         msg+=`_${cit.text}_\n•${cit.author.replace('_', '\\_')} ${cit.date}\n\n`;
     });
 
-    if(pagina.c+1<numPages){
-        //Se non sei nell'ultima pagina stampa la lista con il bottone
-        ctx.replyWithMarkdown(msg, Markup.inlineKeyboard([
-            Markup.callbackButton(`📖Altre citazioni... (${pagina.c+ 1}/${numPages})`, 'n')
-        ]).extra());
+    //Madonna che schifo sta roba, però la lascio visto che va
+    try {
+        if(page.c+1<numPages){
+            ctx.editMessageText(msg, tastoNext);
+        }
+        else{
+            console.log("BASTA CAZZO");
+            ctx.editMessageText(msg);
+            page.c = 0; //Resetta il contatore delle pagine
+            arraydicitazioni = []; //Resetta il cristo.
+        }
+    } catch{
+        if(page.c+1<numPages){
+            ctx.reply(msg, tastoNext);
+        }
+        else{
+            console.log("EIACULAZIONE PRECOCE");
+            ctx.reply(msg);
+            page.c = 0; //Resetta il contatore delle pagine
+            arraydicitazioni = []; //Resetta il cristo.
+        }
+    }
 
-        //Associa il bottone alla funzione
-        bot.action('n', (context)=>{
-            //Cancella la pagina precedente
-            context.deleteMessage();
-            pagina.c++;
-            stampaLista(context, array, pagina);
-        });
-    }
-    else{
-        //Stampa l'ultima pagina senza bottone
-        ctx.replyWithMarkdown(msg);
-        pagina.c = 0; //Resetta il contatore delle pagine
-    }
-} 
+    bot.action('n', (contesto)=>{
+        page.c++;
+        faiCose(contesto, arraydicitazioni, page);
+    });
+}
+
+bot.help((ctx)=>{
+    let helpmsg="Rispondi ad un messaggio col comando /quote per salvare la citazione per i posteri!\nUtilizza il comando /list per vedere la lista delle citazioni";
+    ctx.reply(helpmsg);
+});
 
 bot.launch();
