a3c93de112e6239bc8bffc15b21b769cf8df5cef
AndreaFraschini01 <andrea.fraschini.studenti@isii.it>
2020-04-30 09:48:29 +0200

    Anche questo

---
+++ bot-commands/help.js
@@ -0,0 +1,9 @@
+module.exports = (bot)=>{
+    bot.help((ctx)=>{
+        ctx.replyWithMarkdown(
+"•Rispondi ad un messaggio col comando /quote per salvare la citazione per i posteri!\n\
+•Utilizza il comando /list per vedere la lista delle citazioni.\n\
+•Tagga un membro del gruppo dopo il comando /quotesof per visualizzare tutte le sue citazioni!\n\
+⚠ASSICURATI CHE IO POSSA VEDERE I MESSAGGI⚠");
+    });
+}
---
+++ bot-commands/start.js
@@ -0,0 +1,8 @@
+
+
+
+module.exports = (bot)=>{
+    bot.command('/start', ctx=>{
+        ctx.reply("Ciao! Sono il bot delle citazioni super simpy! Ricordati di darmi i pieni poteri e i mezzi di produzione se no io i messaggi come li vedo eeeh?");
+    });
+}
