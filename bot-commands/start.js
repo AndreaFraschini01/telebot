72b413a96687c4f5651c0e1057ba283872d6f598
AndreaFraschini01 <andrea.fraschini.studenti@isii.it>
2020-04-30 09:48:17 +0200

    Ma che bello questo castello

--- bot-commands/start.js
+++
@@ -1,8 +0,0 @@
-
-
-
-module.exports = (bot)=>{
-    bot.command('/start', ctx=>{
-        ctx.reply("Ciao! Sono il bot delle citazioni super simpy! Ricordati di darmi i pieni poteri e i mezzi di produzione se no io i messaggi come li vedo eeeh?");
-    });
-}
